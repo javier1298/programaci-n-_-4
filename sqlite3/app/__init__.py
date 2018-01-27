f6a8a09fdffabbe96c4ce14772c0f15628ca3da7
javier1298 <ortega.javier98@gmail.com>
2018-01-27 18:08:17 -0500

    Add files via upload

---
+++ sqlite3/app/__init__.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+print('''1.agregar \n2.buscar ''')
+opc = input()
+db = sqlite3.connect('prueba')
+db.create_function('libro', 1,)
+
+if opc == '1':
+
+    cursor = db.cursor()
+    autor = input('autor: ')
+    cpag = input('introdusca la cantidad de paginas del libro: ')
+    public = input('fecha de publicacion: ')
+    cursor.execute('''INSERT INTO libro (autor,cpag,public )VALUES(?,?,?)''', (autor, cpag, public))
+
+elif opc == '2':
+    db = sqlite3.connect('prueba')
+    cursor = db.cursor()
+    bus = input('escriba el nombre del autor: ')
+    r = cursor.execute('SELET autor FROM libro where autor = ?', (bus))
+    print(r)
+
+
+
+db.commit()
+db.close()
+
+
+
